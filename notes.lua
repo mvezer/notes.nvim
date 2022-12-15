c11f6e29846e7167300bca96c2a5353c0e3df112
Matyas Vezer <vezer.m@gmail.com>
2022-12-15 21:02:26 +0100

    clean up shit

--- notes.lua
+++ notes.lua
@@ -1,15 +1,1 @@
-local main = require("telescope._extensions.notes.main")
-
-local fallback_error = { "Falling back to `:Telescope repo list`, but this behavior may change in the future" }
-
-return require("telescope").register_extension({
-    exports = {
-        list = main.list,
-        cached_list = main.cached_list,
-        -- Default command, for now, may change
-        notes = function(opts)
-            vim.api.nvim_echo({ fallback_error }, true, {})
-            main.list(opts)
-        end,
-    },
-})
+require("telescope._extensions.notes.main")
